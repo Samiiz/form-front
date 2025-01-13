c631ab531d6f6967e4e6e31ddc1a1e30333746fd
Samiiz <skboss159@gmail.com>
2025-01-14 00:40:59 +0900

    feat: stats API

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -5,9 +5,8 @@ import IndexPage from "./pages/IndexPage";
 import SignupPage from "./pages/SignupPage";
 import SurveyPage from "./pages/SurveyPage";
 import QuestionPage from "./pages/QuestionPage";
-import StatsPage from "./pages/ResultPage";
+import StatsPage from "./pages/ResultPage"; // ResultPage에서 StatsPage로 수정
 import './App.css';
-
 
 function App() {
   return (
@@ -18,7 +17,7 @@ function App() {
           <Route path="/survey" element={<SurveyPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/question/:id" element={<QuestionPage />} />
-          <Route path="/result" element={<StatsPage />} />
+          <Route path="/result" element={<StatsPage />} /> {/* ResultPage -> StatsPage로 변경 */}
         </Routes>
       </Router>
     </ApiProvider>
